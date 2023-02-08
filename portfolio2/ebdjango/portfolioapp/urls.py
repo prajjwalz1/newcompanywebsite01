8adb10efd8d01853260837afb79a1054b504806d
prajjwalz1 <acharyaprajjwol152@gmail.com>
2023-02-08 09:03:43 +0545

    Initial Commit

---
+++ portfolio2/ebdjango/portfolioapp/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import  re_path
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('', views.send_email, name='send_email'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+    re_path(r'^send-email/$', views.send_email, name='send_email'),
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
